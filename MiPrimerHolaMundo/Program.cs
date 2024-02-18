e26a1573af432917bb093268e8174971c61e62be
Sebastián Castro Saldarriaga <setos02@hotmail.com>
2024-02-17 19:33:45 -0500

    hola mundo

---
+++ MiPrimerHolaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiPrimerHolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Hola Mundo, Como estan?!");
+        }
+    }
+}
